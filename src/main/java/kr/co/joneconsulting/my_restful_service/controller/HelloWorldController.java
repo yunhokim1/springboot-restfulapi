a10349835044d11f413abf45c3813a8453e896b7
kyh <kyh@tigensoft.co.kr>
2024-12-16 17:33:05 +0900

    Spring Boot 3.x 를 이용한 RESTful Web Services 개발 - 섹션2 DispatcherServlet과 프로젝트 동작의 이해, Path Variable 이용 - 가변 URI 사용

--- src/main/java/kr/co/joneconsulting/my_restful_service/controller/HelloWorldController.java
+++ src/main/java/kr/co/joneconsulting/my_restful_service/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package kr.co.joneconsulting.my_restful_service.controller;
 
 import kr.co.joneconsulting.my_restful_service.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +18,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World!");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
