2cc3b317bbfe440eea2220e3f598534aa336aa4d
kyh <kyh@tigensoft.co.kr>
2024-12-17 17:01:46 +0900

    Spring Boot 3.x 를 이용한 RESTful Web Services 개발 - 섹션3 Spring AOP를 이용한 Exception Handling

---
+++ src/main/java/kr/co/joneconsulting/my_restful_service/exception/ExceptionResponse.java
@@ -0,0 +1,16 @@
+package kr.co.joneconsulting.my_restful_service.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExceptionResponse {
+    private Date timestamp;
+    private String message;
+    private String details;
+}
