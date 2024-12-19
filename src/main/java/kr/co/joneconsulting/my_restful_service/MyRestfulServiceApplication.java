324b056302fb55e7a5eb6fa72ef0bbb95f168917
kyh <kyh@tigensoft.co.kr>
2024-12-19 10:51:54 +0900

    Spring Boot 3.x 를 이용한 RESTful Web Services 개발 - 섹션4 다국어 처리를 위한 Internationalization 구현 방법

--- src/main/java/kr/co/joneconsulting/my_restful_service/MyRestfulServiceApplication.java
+++ src/main/java/kr/co/joneconsulting/my_restful_service/MyRestfulServiceApplication.java
@@ -2,13 +2,24 @@ package kr.co.joneconsulting.my_restful_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class MyRestfulServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyRestfulServiceApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
 	}
 
 }
--- src/main/java/kr/co/joneconsulting/my_restful_service/controller/HelloWorldController.java
+++ src/main/java/kr/co/joneconsulting/my_restful_service/controller/HelloWorldController.java
@@ -1,12 +1,23 @@
 package kr.co.joneconsulting.my_restful_service.controller;
 
 import kr.co.joneconsulting.my_restful_service.bean.HelloWorldBean;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     // GET
     // URI - /hello-world
     // @RequestMapping(method=RequestMethod.Get, path="/hello-world")
@@ -23,5 +34,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("greeting.message", null, locale);
     }
 }
